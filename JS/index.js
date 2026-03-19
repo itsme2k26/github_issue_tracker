0e5ffed6c4f327cf8c96829f961821b16325db0e
itsme2k26 <itsme2k26.01@gmail.com>
2026-03-19 06:07:05 +0600

    Load issue function

--- JS/index.js
+++ JS/index.js
@@ -1,12 +1,30 @@
-// for Login Card
-function login(){
-let user = document.getElementById("username").value;
-let pass = document.getElementById("password").value;
+let allIssues = []
 
-    if(user === 'admin' && pass === 'admin123'){
-        alert('Login Successful')
-        window.location.href = 'issue.html'
-    }else{
-        alert('Invalid username or password !')
-    };
+// LOGIN
+function login() {
+    const user = document.getElementById("username").value
+    const pass = document.getElementById("password").value
+
+    if (user === "admin" && pass === "admin123") {
+        window.location.href = "issue.html"
+    } else {
+        alert("Wrong credentials")
+    }
 }
+
+// LOAD ISSUES
+async function loadIssues() {
+
+    const spinner = document.getElementById("spinner")
+    spinner.classList.remove("hidden")
+
+    const res = await fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues")
+    const data = await res.json()
+
+    allIssues = data.data
+
+    displayIssues(allIssues)
+
+    spinner.classList.add("hidden")
+}
+
